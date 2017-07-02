eb8b48e308042f7f095eccc05c8a573562938e16
Guillermo Pascual <guillermo.pascual@outlook.com>
2017-07-02 00:15:32 -0300

    add logger with basic config

--- api/config/config.go
+++ api/config/config.go
@@ -1,8 +1,9 @@
 package config
 
 import (
-	"log"
+	"time"
 
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
@@ -10,19 +11,19 @@ var config *viper.Viper
 
 // Init is an exported method that takes the environment, starts the viper
 // (external lib) and returns the configuration struct.
-func Init(env string) {
+func Init(env string, log *logrus.Logger) {
 	var err error
 	v := viper.New()
 	v.SetConfigType("yaml")
 	v.SetConfigName(env)
-	if env == "test" {
-		v.AddConfigPath("../config/")
-	} else {
-		v.AddConfigPath("config/")
-	}
+	v.AddConfigPath("config/")
 	err = v.ReadInConfig()
 	if err != nil {
-		log.Fatal("error on parsing configuration file")
+		log.WithFields(logrus.Fields{
+			"time": time.Now().Format(time.RFC3339),
+			"env":  env,
+			"err":  err,
+		}).Fatal("Error on parsing configuration file")
 	}
 	config = v
 }
--- api/main.go
+++ api/main.go
@@ -5,10 +5,21 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/sirupsen/logrus"
+
 	"bitbucket.org/gpascual2/gin-seed/api/config"
 )
 
+var log = logrus.New()
+
+func init() {
+	// Config logger
+	log.Formatter = new(logrus.TextFormatter)
+
+}
+
 func main() {
+	var err error
 	var env string
 	flag.StringVar(&env, "e", "dev", "Working environment. Default: dev")
 	flag.Usage = func() {
@@ -17,8 +28,25 @@ func main() {
 	}
 	flag.Parse()
 	fmt.Println("Environment: ", env)
-	config.Init(env)
+	config.Init(env, log)
 
+	// Read config
 	c := config.GetConfig()
-	fmt.Printf("HTTP Port: %v\n", c.Get("server.port"))
+
+	// Set configed log level
+	cLogLvl, err := logrus.ParseLevel(c.GetString("env.log_level"))
+	if err != nil {
+		cLogLvl = logrus.WarnLevel // Default to Warning
+	}
+	log.Level = cLogLvl
+
+	// Log initial config
+	log.WithFields(logrus.Fields{"env": env}).Info("Environment set")
+	log.WithFields(logrus.Fields{"log_level": c.GetString("env.log_level")}).Info("Log level set")
+
+	// HTTP Port to use
+	log.WithFields(logrus.Fields{
+		"port": c.Get("server.port"),
+	}).Info("HTTP Port configed")
+
 }
